013d577da761b299e6334453b2331fd595a41cb9
Peter Tran-Truong <peter.dai.truong@gmail.com>
2023-10-04 14:14:06 -0700

    Create footer component

---
+++ src/component/Footer.tsx
@@ -0,0 +1,18 @@
+class Footer {
+  render = () => {
+    return (
+      <>
+        <div className="footer-container">
+          <div className="footer">
+            Copyright © 2023 pdtru&nbsp
+            <a href="https://github.com/pdtru">
+              <i className="fa-brands fa-github"></i>
+            </a>
+          </div>
+        </div>
+      </>
+    );
+  };
+}
+
+export default Footer;
