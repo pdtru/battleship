85f345cc50950e78bc5517d03e6b92d2f720421c
Peter Tran-Truong <peter.dai.truong@gmail.com>
2023-10-05 16:40:15 -0700

    Add Font Awesome icon library

--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -1,18 +1,22 @@
-class Footer {
-  render = () => {
-    return (
-      <>
-        <div className="footer-container">
-          <div className="footer">
-            Copyright © 2023 pdtru&nbsp
-            <a href="https://github.com/pdtru">
-              <i className="fa-brands fa-github"></i>
-            </a>
-          </div>
+import { CSSProperties } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+
+const footerContainerStyle: CSSProperties = {};
+
+const Footer = () => {
+  return (
+    <>
+      <div className="footer-container" style={footerContainerStyle}>
+        <div className="footer">
+          Copyright © 2023 pdtru{' '}
+          <a href="https://github.com/pdtru">
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
         </div>
-      </>
-    );
-  };
-}
+      </div>
+    </>
+  );
+};
 
 export default Footer;
