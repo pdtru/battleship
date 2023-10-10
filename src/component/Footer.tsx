81446e4d437db385eadb88122bee04bff7001ca1
Peter Tran-Truong <peter.dai.truong@gmail.com>
2023-10-10 14:28:32 -0700

    Style footer component

--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -1,14 +1,42 @@
-import { CSSProperties } from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import TextComponent from './TextComponent';
 
-const Footer: React.FC = () => {
+const Footer = () => {
+  const [isHover, setIsHover] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHover(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHover(false);
+  };
+
+  const footerContainerStyles = {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: '4px',
+    fontSize: '16px',
+  };
+
+  const iconStyles = {
+    fontSize: '20px',
+    color: isHover ? '#8a77a5' : '#6e5494',
+  };
+
   return (
     <>
-      <div>
+      <div style={footerContainerStyles}>
         <TextComponent text="Copyright © 2023 pdtru" />
-        <a href="https://github.com/pdtru">
+        <a
+          style={iconStyles}
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+          href="https://github.com/pdtru"
+        >
           <FontAwesomeIcon icon={faGithub} />
         </a>
       </div>
