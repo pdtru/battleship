2f8ea4fc98254a4c80131dfd31ddf903acef5787
Peter Tran-Truong <peter.dai.truong@gmail.com>
2023-10-11 16:14:45 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Footer from './component/Footer';
 import GameContainer from './component/GameContainer';
+import Header from './component/Header';
 
 function App() {
   return (
     <>
+      <Header />
       <GameContainer />
       <Footer />
     </>
